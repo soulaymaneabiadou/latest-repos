3adbce0012621b7fb6b548f6f9d7384a233a6c3d
Soulaymane Abiadou <abiadou.soulaymane@gmail.com>
2020-04-03 18:00:11 +0100

    Initial server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config();
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.listen(
+  process.env.PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  )
+);
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Unhandled Error: ${err.message}`);
+});
